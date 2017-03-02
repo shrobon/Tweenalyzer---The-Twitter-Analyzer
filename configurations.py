84e0d90f11f0eb0ad6c2c4d55cb4face4966cda4
shrobon <shrobon.biswas@gmail.com>
2017-03-01 23:14:39 -0700

    Should always return a string response instead of an int

---
+++ configurations.py
@@ -0,0 +1,2 @@
+consumer_key = "WPuAVx1z2gsMqEWQw3vNmNszm"
+consumer_secret = "3nVgS6y7FFDwq7y8sEuZMWygmeAXVU7tI40j1IEZ2ygxwCxAZF"
--- tweets.py
+++ tweets.py
@@ -5,4 +5,24 @@
 #The necessary data will be returned to the server for parsing 
 #The script will just accept the Query String
 
+import tweepy
+import numpy as np
+import configurations 
+import pandas as pd
+
+def QueryTwitter(search_string):
+	
+	#Fetching the Configuration Settings
+	key = configurations.consumer_key
+	secret = configurations.consumer_secret
+
+	#Authenticating ::
+	#------Receiving Access Tokens
+	access_token = tweepy.OAuthHandler(consumer_key=key,consumer_secret=secret)
+	#------Instantiating the API with our Access Token
+	api = tweepy.API(access_token)
+
+	results = api.search(q=search_string)
+	return len(results)
+
 
