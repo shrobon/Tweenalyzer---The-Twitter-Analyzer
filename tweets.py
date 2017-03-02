f8b71d9d4bc793a073a1f0cb8502efd8e2ed2339
shrobon <shrobon.biswas@gmail.com>
2017-03-01 21:32:27 -0700

    Server Code :: Tweepy installed

---
+++ tweets.py
@@ -0,0 +1,8 @@
+#__Author__ : Shrobon Biswas
+
+#__Description__:
+#This script will use twitter api
+#The necessary data will be returned to the server for parsing 
+#The script will just accept the Query String
+
+
