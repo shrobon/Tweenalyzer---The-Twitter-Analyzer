b816013cc4304ee324c85d5de13cc32307a6ba7d
shrobon <shrobon.biswas@gmail.com>
2017-03-03 02:14:20 -0700

    Added KeeN.io Dashboards to display data ~~ Looks Perfect Now git status

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	tweetsDataframe=QueryTwitter("Python")
-
-	return tweetsDataframe.to_html()
-	#return tweetsDataframe
-	#return "Hello World"
+	#tweetsDataframe=QueryTwitter("Python")
+	#return tweetsDataframe.to_html()
+	return render_template('index.html')
 
 
 if __name__ == "__main__":
