de079272d870d7dbed351fdb2dcb9b804af723b8
Shrobon Biswas <shrobon.biswas@gmail.com>
2019-03-06 13:43:51 -0500

    Updating Configurations

--- configurations.py
+++ configurations.py
@@ -1,6 +1,6 @@
-consumer_key = "jwwBd0sOywLKeKjH266lodDfY"
-consumer_secret = "0vZ0rAPloSRnv8FF5HbqN8nwBdqfmU32uqa5KGPyoNOiD3uCvd"
-access_token = "834466544478457856-voDajsQWfMoXkaEca7wQq9xJTx53gBE"
-access_secret = "xXbLADiMuiatA86Y5YLeaKwck952gHEjbsCgJzemvfwXS"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_secret = ""
 
-google_maps_key = 'AIzaSyAKEVb5_f4COm5uPQhOPer9gsbdW4fb75o'
+google_maps_key = ''
